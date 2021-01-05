4742005990c265c23ca5fd03700a52d5402344ec
BianLee <bianlee2004@gmail.com>
2021-01-04 17:59:33 -0800

    potd fix

--- src/frames/firstframe/frames/login.js
+++ src/frames/firstframe/frames/login.js
@@ -14,7 +14,9 @@ export default class LoginFirstFrame extends React.Component {
         return (
             <>
             <section id="intro" className="c-paper c-paper--no-border-radius-bottom h-mb-0">
-                <LoginSetupContainer/>     
+              <LoginSetupContainer/>     
+              			  <p>Thank you for your interest in InterSTEM Bowl! To register and obtain an account, please fill out the form below.</p>
+<a href="https://nahn5f40e7j.typeform.com/to/dbH76h3J" target="_blank">https://nahn5f40e7j.typeform.com/to/dbH76h3J</a>   
             </section>
     
             </>
