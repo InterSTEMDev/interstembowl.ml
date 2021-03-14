4d5f6c65546d7a447b10e4cb8eee0425c3c5da7a
Bian <bianlee2004@gmail.com>
2021-03-13 18:18:52 -0800

    Added Wolfram as Sponsor

--- src/frames/firstframe/frames/root.js
+++ src/frames/firstframe/frames/root.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "../../../styles/style.css"
 import aopsLogo from "../../../images/aops-logo.svg"
+import wolframLogo from "../../../images/wolfram.png"
 
 export default class RootFirstFrame extends React.Component {
     constructor() {
@@ -13,25 +14,26 @@ export default class RootFirstFrame extends React.Component {
     render() {
         return (
             <>
-            
+
             <section id="intro" className="c-paper c-paper--no-border-radius-bottom h-mb-0">
                 <p>InterSTEM Bowl is a unique STEM competition that allows students to showcase their creativity and test their knowledge in a variety of STEM fields. Over $1000 in cash prizes will be split between 15+ winners. <a href="https://nahn5f40e7j.typeform.com/to/dbH76h3J">Registriation </a> (also on login page) for InterSTEM Bowl is currently open. The competition will be held in late March with the following categories: </p>
                 <p>  - Quiz category: a science bowl-like exam on subjects such as biology, chemistry, physics, math, and computer science</p>
                 <p>  - Video category: a video submission on a designated prompt graded on content and delivery</p>
-                <p>  - Writing category: an article submission on a designated prompt graded on content, writing style, and topic adherence</p> 
-                
+                <p>  - Writing category: an article submission on a designated prompt graded on content, writing style, and topic adherence</p>
+
             </section>
 
             <section id="intro" className="c-paper colored-bg c-paper--no-border-radius-bottom h-mb-0">
                 <h2 className="section-title">Our Sponsors</h2>
                 <div className="sponsors-container">
                   <div className="sponsor">
+                    <img style={{width: "10vw"}} src={wolframLogo}></img>
                     <img src={aopsLogo}></img>
-                  </div>  
-                </div>                
+                  </div>
+                </div>
             </section>
-            
-             </> 
+
+             </>
         )
     }
 }
