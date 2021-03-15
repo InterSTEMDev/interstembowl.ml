e47adf32a1c280c24bc910edc730b2e3845b1b1b
Bian <bianlee2004@gmail.com>
2021-03-14 23:59:52 -0700

    prompt released

--- src/frames/firstframe/frames/root.js
+++ src/frames/firstframe/frames/root.js
@@ -1,41 +1,65 @@
-import React from "react"
-import "../../../styles/style.css"
-import aopsLogo from "../../../images/aops-logo.svg"
-import wolframAlphaLogo from "../../../images/wolframalpha-logo.png"
+import React from "react";
+import "../../../styles/style.css";
+import aopsLogo from "../../../images/aops-logo.svg";
+import wolframAlphaLogo from "../../../images/wolfram.png";
+import prompt from "../../../images/prompts.png";
 
 export default class RootFirstFrame extends React.Component {
-    constructor() {
-        super()
-        this.state = {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-        }
-    }
+  render() {
+    return (
+      <>
+        <section className="c-paper intro c-paper--no-border-radius-bottom h-mb-0">
+          <div className="sponsors-container">
+            <div className="sponsor">
+              <img src={prompt} style={{ width: "40vw" }}></img>
+            </div>
+          </div>
+          <p>
+            <br></br>
+            <br></br>
+            InterSTEM Bowl is a unique STEM competition that allows students to
+            showcase their creativity and test their knowledge in a variety of
+            STEM fields. Over $1000 in cash prizes will be split between 15+
+            winners.{" "}
+            <a href="https://nahn5f40e7j.typeform.com/to/dbH76h3J">
+              Registriation{" "}
+            </a>{" "}
+            (also on login page) for InterSTEM Bowl is currently open. The
+            competition will be held in late March with the following
+            categories:{" "}
+          </p>
+          <p>
+            {" "}
+            - Quiz category: a science bowl-like exam on subjects such as
+            biology, chemistry, physics, math, and computer science
+          </p>
+          <p>
+            {" "}
+            - Video category: a video submission on a designated prompt graded
+            on content and delivery
+          </p>
+          <p>
+            {" "}
+            - Writing category: an article submission on a designated prompt
+            graded on content, writing style, and topic adherence
+          </p>
+        </section>
 
-    render() {
-        return (
-            <>
-
-            <section className="c-paper intro c-paper--no-border-radius-bottom h-mb-0">
-                <p>InterSTEM Bowl is a unique STEM competition that allows students to showcase their creativity and test their knowledge in a variety of STEM fields. Over $1000 in cash prizes will be split between 15+ winners. <a href="https://nahn5f40e7j.typeform.com/to/dbH76h3J">Registriation </a> (also on login page) for InterSTEM Bowl is currently open. The competition will be held in late March with the following categories: </p>
-                <p>  - Quiz category: a science bowl-like exam on subjects such as biology, chemistry, physics, math, and computer science</p>
-                <p>  - Video category: a video submission on a designated prompt graded on content and delivery</p>
-                <p>  - Writing category: an article submission on a designated prompt graded on content, writing style, and topic adherence</p>
-
-            </section>
-
-            <section className="c-paper colored-bg c-paper--no-border-radius-bottom h-mb-0">
-                <h2 className="section-title">Our Sponsors</h2>
-                <div className="sponsors-container">
-                  <div className="sponsor">                    
-                    <img src={aopsLogo}></img>
-                  </div>
-                <div className="sponsor">
-                  <img src={wolframAlphaLogo}></img>
-                </div>
-                </div>
-            </section>
-
-             </>
-        )
-    }
+        <section className="c-paper colored-bg c-paper--no-border-radius-bottom h-mb-0">
+          <h2 className="section-title">Our Sponsors</h2>
+          <div className="sponsors-container">
+            <div className="sponsor">
+              <img style={{ width: "10vw" }} src={wolframAlphaLogo}></img>
+              <img style={{ width: "25vw" }} src={aopsLogo}></img>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
